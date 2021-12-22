8865aacae2af24714c2cb80841d266f1b987d180
FfEzT <vl.al.kim@mail.ru>
2021-12-22 20:28:50 +0500

    0.1.4.5

--- client/src/main_window/js/new_user.js
+++ client/src/main_window/js/new_user.js
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Depesha.
+
+ * Depesha is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * Depesha is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with Depesha.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // delete old info (list of friends, key, messages)
 data.main('delete_data')
 data.red_point.to_null()
