0346bbd98fcc325b2d2a96e6aee25d6e3e700f08
FfEzT <vl.al.kim@mail.ru>
2022-01-24 16:49:13 +0500

    WebRTC "it's working" presave

--- src/web/main.js
+++ src/web/main.js
@@ -22,7 +22,59 @@ const iceServers = [
         url: 'turn:192.158.29.39:3478?transport=tcp',
         credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
         username: '28224511:1379330808'
-    }
+    },
+    {
+        url: 'turn:numb.viagenie.ca',
+        credential: 'muazkh',
+        username: 'webrtc@live.com'
+    },
+    {
+        url: 'turn:192.158.29.39:3478?transport=udp',
+        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        username: '28224511:1379330808'
+    },
+    {
+        url: 'turn:192.158.29.39:3478?transport=tcp',
+        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        username: '28224511:1379330808'
+    },
+    {
+        url: 'turn:turn.bistri.com:80',
+        credential: 'homeo',
+        username: 'homeo'
+     },
+     {
+        url: 'turn:turn.anyfirewall.com:443?transport=tcp',
+        credential: 'webrtc',
+        username: 'webrtc'
+    },
+    {
+        "urls": [
+        "turn:13.250.13.83:3478?transport=udp"
+        ],
+        "username": "YzYNCouZM1mhqhmseWk6",
+        "credential": "YzYNCouZM1mhqhmseWk6"
+    },
+    {
+        url: 'turn:relay.backups.cz',
+        credential: 'webrtc',
+        username: 'webrtc'
+    },
+    {
+        url: 'turn:relay.backups.cz?transport=tcp',
+        credential: 'webrtc',
+        username: 'webrtc'
+    },
+    { url: 'stun:stun.gmx.net:3478' },
+{ url: 'stun:stun.l.google.com:19302' },
+{ url: 'stun:stun1.l.google.com:19302' },
+{ url: 'stun:stun2.l.google.com:19302' },
+{ url: 'stun:stun3.l.google.com:19302' },
+{url: 'stun.justvoip.com:3478'},
+{url: 'stun.lugosoft.com:3478'},
+{url: 'stun.lugosoft.com:3478'},
+{url: 'stun.myvoiptraffic.com:3478'},
+{url: 'stun.node4.co.uk:3478'}
 ]
 
 const element = {
